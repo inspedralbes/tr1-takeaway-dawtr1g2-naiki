aef89b4983c3d1a50ab114d5c14efcc63cf5614d
a22lorcrinor <a22lorcrinor@inspedralbes.cat>
2023-10-24 08:43:43 +0200

    Començant a crear la part api de comandes

---
+++ app/Http/Controllers/ControllerComanda.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comanda;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ControllerComanda extends Controller
+{
+    public function getComanda()
+    {
+        $comandes = Comanda::all();
+        return $comandes;
+    }
+
+    public function createComanda(Request $request)
+    {   
+        $sabata = DB::table('comanda')
+                ->latest()
+                ->first();
+        if ($sabata != null){
+            $idComanda = $sabata->idComanda + 1;
+
+        }else{
+            $idComanda = 0;
+        }
+        $numItem = 1;
+        foreach ($request->sabates as $sabata) {
+            $comanda = new Comanda();
+            $comanda->idComanda = $idComanda;
+            $comanda->numItem = $numItem;
+            $numItem++;
+            $comanda->marca = $sabata->marca;
+            $comanda->model = $sabata->model;
+            $comanda->genere = $sabata->genere;
+            $comanda->talla = $sabata->talla;
+            $comanda->imatge = $sabata->imatge;
+            $comanda->color = $sabata->color;
+            $comanda->quantitat = $sabata->quantitat;
+            $comanda->estat = "En preparacio";
+            $comanda->save();
+
+        }
+    }
+}
