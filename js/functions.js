9d89b27a4d0a71f52441b0869c07916a24e46437
a22lorcrinor <a22lorcrinor@inspedralbes.cat>
2023-10-25 11:01:24 +0200

    Canvis a com funciona i es veu la cesta

--- js/functions.js
+++ js/functions.js
@@ -80,16 +80,18 @@ createApp({
         cesta() {
             let openShopping = document.querySelector('.shopping');
             let closeShopping = document.querySelector('.closeShopping');
+             let cesta = document.querySelector(".cesta-active")
+          
 
-
-            openShopping.addEventListener('click', () => {
-                document.body.classList.add('active');
-            })
-
-            closeShopping.addEventListener('click', () => {
-                document.body.classList.remove('active');
-
-            })
+                openShopping.addEventListener('click', () => {
+                    document.querySelector(".cesta").classList.add("cesta-active");
+                })
+    
+                closeShopping.addEventListener('click', () => {
+                    document.querySelector(".cesta").classList.remove("cesta-active");
+    
+                })
+            
         }
 
     },
