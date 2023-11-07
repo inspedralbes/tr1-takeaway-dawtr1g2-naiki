7c3e31af02098d9659d312ad11908e557795d9a1
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2023-11-07 12:41:17 +0100

    Funcionalitat d'enviar qr

--- app/Http/Controllers/ControllerComanda.php
+++ app/Http/Controllers/ControllerComanda.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
 class ControllerComanda extends Controller
 {
     public function getComanda()
@@ -20,8 +21,8 @@ class ControllerComanda extends Controller
     }
 
     public function createComanda(Request $request)
-    {   
-        
+    {
+
         $numItem = 1;
         $payload = json_decode($request->getContent(), true);
         $sabates = $payload[1]["sabates"];
@@ -33,12 +34,12 @@ class ControllerComanda extends Controller
 
 
         $comanda = DB::table('comandas')
-                ->latest()
-                ->first();
-        if ($comanda != null){
+            ->latest()
+            ->first();
+        if ($comanda != null) {
             $idComanda = $comanda->id;
 
-        }else{
+        } else {
             $idComanda = 0;
         }
         foreach ($sabates as $sabata) {
@@ -47,74 +48,77 @@ class ControllerComanda extends Controller
             $lineaComanda->numItem = $numItem;
             $numItem++;
 
-            $sabataQuery = Sabates::find( $sabata["id"]);
+            $sabataQuery = Sabates::find($sabata["id"]);
 
-             $lineaComanda->preu = $sabataQuery->preu;
-             $lineaComanda->marca  = $sabata["marca"];
-             $lineaComanda->model =  $sabata["model"];
-            $lineaComanda->genere =  $sabata["genere"];
-            $lineaComanda->talla =  $sabata["talla"];
-             $lineaComanda->imatge =  $sabata["imatge"];
-             $lineaComanda->color =  $sabata["color"];
-             $lineaComanda->quantitat =  $sabata["quantitat"];
+            $lineaComanda->preu = $sabataQuery->preu;
+            $lineaComanda->marca = $sabata["marca"];
+            $lineaComanda->model = $sabata["model"];
+            $lineaComanda->genere = $sabata["genere"];
+            $lineaComanda->talla = $sabata["talla"];
+            $lineaComanda->imatge = $sabata["imatge"];
+            $lineaComanda->color = $sabata["color"];
+            $lineaComanda->quantitat = $sabata["quantitat"];
 
-            if ( $sabata["quantitat"]<=0) {
-                 $lineaComanda->quantitat =  -$sabata["quantitat"];
-             }
-         $lineaComanda->save();
+            if ($sabata["quantitat"] <= 0) {
+                $lineaComanda->quantitat = -$sabata["quantitat"];
+            }
+            $lineaComanda->save();
 
         }
-        
-        $lineasComanda = DB::table('linea_comandas')->where('idComanda','=', $idComanda)->get();
-        $comanda = Comanda::find('id',$idComanda);
+
+        $lineasComanda = DB::table('linea_comandas')->where('idComanda', '=', $idComanda)->get();
+        $comanda = Comanda::find($idComanda);
         //Mail::to($email)->send(new \App\Mail\Comanda($comanda));
         $qrData = [
             'Ticket ID' => $comanda->id,
             'Email' => $comanda->usuari,
-          ];
-          $jsonQrCode = json_encode($qrData);
-          $qrCode = QrCode::size(300)->generate($jsonQrCode);
-      
-          $qrCodeFileName = 'ticket_' . $comanda->id . '_qr.svg';
-      
-          try {
-            Storage::disk('pdf')->put('/qr' . $qrCodeFileName, $qrCode);
-      
+        ];
+        $jsonQrCode = json_encode($qrData);
+        $qrCode = QrCode::size(300)->generate($jsonQrCode);
+
+        $qrCodeFileName = 'ticket_' . $comanda->id . '_qr.svg';
+
+        try {
+            Storage::disk('pdf')->put('qr/' . $qrCodeFileName, $qrCode);
+
             $imagePath = 'qr/' . $qrCodeFileName;
             $comanda->qr = $imagePath;
             $comanda->save();
-          } catch (\Exception $e) {
+        } catch (\Exception $e) {
             \Log::error('Error al almacenar el código QR: ' . $e->getMessage());
             return response()->json(['error' => 'Error al almacenar el código QR'], 500);
-          }
+        }
 
         $data["email"] = $email;
         $data["title"] = "Gràcies per la teva compra!";
         $data["body"] = "Rebut de la compra";
-        $data["comanda"] = $lineasComanda;
-        
+        $data["lineasComanda"] = $lineasComanda;
+        $data["comanda"] = $comanda;
+
         $pdf = Pdf::loadView('comanda', $data);
-  
-        Mail::send('bodycomanda', $data, function($message)use($data, $pdf) {
+
+        Mail::send('bodycomanda', $data, function ($message) use ($data, $pdf) {
             $message->to($data["email"], $data["email"])
-                    ->subject($data["title"])
-                    ->attachData($pdf->output(), "text.pdf");
+                ->subject($data["title"])
+                ->attachData($pdf->output(), "text.pdf");
         });
     }
-    
 
-    public function canviarEstatComanda(Request $request){
-        
+
+    public function canviarEstatComanda(Request $request)
+    {
+
         $idComanda = $request->idComanda;
 
         $nouEstat = $request->nouEstat;
         DB::table('comandas')
-        ->where('id', $idComanda)
-        ->update(['estat' => $nouEstat]);
+            ->where('id', $idComanda)
+            ->update(['estat' => $nouEstat]);
 
     }
 
-    public function veuraComanda(){
+    public function veuraComanda()
+    {
 
     }
 }
