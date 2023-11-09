7c56b771e3db67dba684d028bcb7b6635e7babdc
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2023-11-09 11:55:25 +0100

    Arreglado otro error del get comanda

--- app/Http/Controllers/ControllerComanda.php
+++ app/Http/Controllers/ControllerComanda.php
@@ -26,6 +26,10 @@ class ControllerComanda extends Controller
             //Return if the user is logged in or not from the token
             [$id, $token] = explode('|', $checkToken, 2);
             $id = ltrim($id, '{'); 
+                $token = rtrim($token, '}'); 
+
+                
+                
             $accessToken = PersonalAccessToken::find($id);
             if ($accessToken != null) {
                 if (hash_equals($accessToken->token, hash('sha256', $token))) {
