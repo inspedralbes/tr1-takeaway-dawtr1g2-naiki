c116e85a49dee020eaeb3ce5dd37ea353e3d3d87
loriscrisafo <a22lorcrinor@inspedralbes.cat>
2023-11-09 22:37:40 +0100

    Esborrat tot per fer merges sense problemes

--- front/js/communicatonManagar.js
+++
@@ -1,7 +0,0 @@
-export async function  getSabates() {
-     //Link per el fetch, si estas a labs es canvia la variable linkFetch a ../ si es en local es canvia athis.fetchLink+ 
-    let fetchLink = "http://localhost:8000";
-    const response = await fetch(fetchLink+"/api/sabates");
-    const peliculas = await response.json();
-    return peliculas;
-}
